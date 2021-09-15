07a47342ae0865c7d55a6fb191aa0de8c338a513
Roman Leshchyshyn <app.stormer2017@gmail.com>
2021-09-15 10:12:47 +0300

    made eng|ua switch

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage();
-});
+$(function() {
+    $('.info-tabs__item').on('click', function(e) {
+        e.preventDefault();
+        $('.info-tabs__item').removeClass('info-tabs__item--active');
+        $(this).addClass('info-tabs__item--active')
+        $('.info-tabs__content').removeClass('info-tabs__content--active');
+        $($(this).attr('href')).addClass('info-tabs__content--active');
+    })
+    $('.chg-lang__ua').on('click', function() {
+        $('body').removeClass('eng').addClass('ua')
+    })
+    $('.chg-lang__eng').on('click', function() {
+        $('body').removeClass('ua').addClass('eng')
+    })
+})
